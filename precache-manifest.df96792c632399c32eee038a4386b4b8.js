d80e90f6fc3a0986b94aad23cc97273aa93972e6
tkrogers3 <tkrogers3@gmail.com>
2020-04-08 13:42:19 -0400

    Updates

---
+++ precache-manifest.df96792c632399c32eee038a4386b4b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81b79b3c7b6cbfd27f57595467dded79",
+    "url": "/puzzle-slider/index.html"
+  },
+  {
+    "revision": "e0d157c1a7043cacd159",
+    "url": "/puzzle-slider/static/css/2.7ad897a5.chunk.css"
+  },
+  {
+    "revision": "7bb3e91d141710ecfe4f",
+    "url": "/puzzle-slider/static/css/main.796a7a14.chunk.css"
+  },
+  {
+    "revision": "e0d157c1a7043cacd159",
+    "url": "/puzzle-slider/static/js/2.92f30bf2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/puzzle-slider/static/js/2.92f30bf2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bb3e91d141710ecfe4f",
+    "url": "/puzzle-slider/static/js/main.91c943d5.chunk.js"
+  },
+  {
+    "revision": "018a5ea7e7a38053cc56",
+    "url": "/puzzle-slider/static/js/runtime-main.737f2ff1.js"
+  }
+]);
